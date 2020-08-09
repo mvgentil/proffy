218679d48cd9e912d25a5844b15874fca3cb041c
Matheus Gentil <matheusvgentil@gmail.com>
2020-08-09 19:12:27 -0300

    Changed submodule to directory

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,20 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface InputPorps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+}
+
+
+const Input: React.FC<InputPorps> = ({ label, name, ...rest }) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type="text" id={name} {...rest} />
+  </div>
+  );
+}
+
+export default Input;
